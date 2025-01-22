a5852f262ab3bbc0949299a21b5256c5b32203dd
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-01-22 15:28:56 -0300

    feat(service): implementar ClienteService para gerenciamento de clientes
    Criada a classe `ClienteService` para gerenciar as operações relacionadas aos clientes.
    Incluídas as seguintes funcionalidades:
    - `criarCliente`: cria um novo cliente a partir de um `ClienteRequest` e retorna um `ClienteResponse`.
    - `lerClientes`: retorna uma lista de todos os clientes no formato `ClienteResponse`.
    - `obterClientePeloId`: busca um cliente pelo ID, lançando exceção caso não seja encontrado.
    - `atualizarCliente`: atualiza os dados de um cliente existente com base no ID e no `ClienteRequest`.
    - `deletarCliente`: remove um cliente do banco de dados com base no ID.
    
    Adicionados métodos auxiliares:
    - `obterClienteDeClienteRequest`: converte um `ClienteRequest` em um objeto `Cliente`.
    - `obterClienteResponseDeCliente`: converte um objeto `Cliente` em um `ClienteResponse`.
    
    Integrado com o `ClienteRepository` para persistência dos dados.

---
+++ src/main/java/com/zup/pizzaria/services/ClienteService.java
@@ -0,0 +1,57 @@
+package com.zup.pizzaria.services;
+
+import com.zup.pizzaria.dtos.requestsDtos.ClienteRequest;
+import com.zup.pizzaria.dtos.responseDtos.ClienteResponse;
+import com.zup.pizzaria.models.Cliente;
+import com.zup.pizzaria.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public ClienteResponse criarCliente(ClienteRequest clienteRequest) {
+        Cliente cliente = obterClienteDeClienteRequest(clienteRequest);
+        clienteRepository.save(cliente);
+        return obterClienteResponseDeCliente(cliente);
+    }
+
+    public List<ClienteResponse> lerClientes() {
+        List<Cliente> clientes = clienteRepository.findAll();
+        return clientes.stream()
+                .map(this::obterClienteResponseDeCliente)
+                .toList();
+    }
+
+    public Cliente obterClientePeloId(Long clienteId) {
+        return clienteRepository.findById(clienteId)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado"));
+    }
+
+    public ClienteResponse atualizarCliente(Long id, ClienteRequest request){
+        Cliente cliente = obterClientePeloId(id);
+        cliente.setNome(request.getNome());
+        cliente.setEmail(request.getEmail());
+        cliente.setTelefone(request.getTelefone());
+        clienteRepository.save(cliente);
+        return obterClienteResponseDeCliente(cliente);
+    }
+
+    public void deletarCliente(Long id){
+        Cliente cliente = obterClientePeloId(id);
+        clienteRepository.delete(cliente);
+    }
+
+    private Cliente obterClienteDeClienteRequest(ClienteRequest clienteRequest) {
+        return new Cliente(clienteRequest.getNome(), clienteRequest.getEmail(), clienteRequest.getTelefone());
+    }
+
+    private ClienteResponse obterClienteResponseDeCliente(Cliente cliente) {
+        return new ClienteResponse(cliente.getId(), cliente.getNome(), cliente.getEmail(), cliente.getTelefone());
+    }
+}
