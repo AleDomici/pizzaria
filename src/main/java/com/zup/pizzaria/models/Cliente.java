65abc2133f913f8093297805d9ba07c16ab02e7f
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-01-22 15:25:41 -0300

    feat(models): adicionar atributo telefone à entidade Cliente Adicionado o atributo `telefone` à classe `Cliente` para armazenar o número de telefone do cliente. Incluído o respectivo getter, setter e ajustado o construtor para suportar o novo atributo. Também foi adicionado um construtor padrão para a classe

--- src/main/java/com/zup/pizzaria/models/Cliente.java
+++ src/main/java/com/zup/pizzaria/models/Cliente.java
@@ -5,13 +5,27 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+
 @Entity
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
+    private String telefone;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
 
     public Long getId() {
         return id;
@@ -37,8 +51,11 @@ public class Cliente {
         this.email = email;
     }
 
-    public Cliente(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
     }
 }
