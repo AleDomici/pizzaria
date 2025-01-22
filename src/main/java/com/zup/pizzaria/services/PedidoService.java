2ac7e0393cf66fc4aacc40a3bfec071a01099fbd
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-01-22 16:15:33 -0300

    refactor(service): refatorar PedidoService para uso de novos DTOs e integração com ClienteService
    - Substituído PedidoDTO por PedidoRequest e PedidoResponse para separar entrada e saída.
    - Integrado ClienteService para centralizar a lógica de cliente.
    - Adicionados métodos para leitura, atualização e exclusão de pedidos.
    - Criados métodos auxiliares para conversão entre entidades e DTOs.
    - Melhorada a separação de responsabilidades e reutilização de código.

--- src/main/java/com/zup/pizzaria/services/PedidoService.java
+++ src/main/java/com/zup/pizzaria/services/PedidoService.java
@@ -1,31 +1,68 @@
 package com.zup.pizzaria.services;
 
-import com.zup.pizzaria.dtos.PedidoDTO;
+import com.zup.pizzaria.dtos.requestsDtos.ClienteRequest;
+import com.zup.pizzaria.dtos.requestsDtos.PedidoRequest;
+import com.zup.pizzaria.dtos.responseDtos.ClienteResponse;
+import com.zup.pizzaria.dtos.responseDtos.PedidoResponse;
 import com.zup.pizzaria.models.Cliente;
 import com.zup.pizzaria.models.Pedido;
 import com.zup.pizzaria.repository.ClienteRepository;
 import com.zup.pizzaria.repository.PedidoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PedidoService {
     private final PedidoRepository pedidoRepository;
-    private final ClienteRepository clienteRepository;
+    private final ClienteService clienteService;
 
-    public PedidoService(PedidoRepository pedidoRepository, ClienteRepository clienteRepository) {
+    public PedidoService(PedidoRepository pedidoRepository, ClienteService clienteService) {
         this.pedidoRepository = pedidoRepository;
-        this.clienteRepository = clienteRepository;
+        this.clienteService = clienteService;
     }
 
-    public PedidoDTO criarPedido(Pedido pedido) {
-        // Salva pedido
+    public PedidoResponse criarPedido(PedidoRequest pedidoRequest) {
+        Cliente cliente = clienteService.obterClientePeloId(pedidoRequest.getClienteId());
+        Pedido pedido = obterPedidoDePedidoRequest(pedidoRequest);
         pedidoRepository.save(pedido);
-
-        // Obtenho cliente
-        Cliente cliente = clienteRepository
-                .findById(pedido.getClienteId())
-                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
-
-        return new PedidoDTO(cliente.getNome(), cliente.getEmail(), pedido.getDescricao());
+        return obterPedidoResponseDePedido(pedido);
     }
+
+    public List<PedidoResponse> lerPedidos() {
+        List<Pedido> pedidos = pedidoRepository.findAll();
+        return pedidos.stream()
+                .map(this::obterPedidoResponseDePedido)
+                .toList();
+    }
+
+    public PedidoResponse atualizarPedido(Long id, PedidoRequest request){
+        Pedido pedido = obterPedidoPeloId(id);
+        pedido.setDescricao(request.getDescricao());
+        pedido.setClienteId(request.getClienteId());
+        pedido.setValorTotal(request.getValorTotal());
+        pedidoRepository.save(pedido);
+        return obterPedidoResponseDePedido(pedido);
+    }
+
+    public Pedido obterPedidoPeloId(Long id){
+        return pedidoRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Pedido não encontrado"));
+    }
+
+    public void deletarPedido(Long id){
+        Pedido pedido = obterPedidoPeloId(id);
+        pedidoRepository.delete(pedido);
+    }
+
+    private Pedido obterPedidoDePedidoRequest(PedidoRequest pedidoRequest) {
+        return new Pedido(pedidoRequest.getDescricao(), pedidoRequest.getClienteId(), pedidoRequest.getValorTotal());
+    }
+
+    private PedidoResponse obterPedidoResponseDePedido(Pedido pedido) {
+        Cliente cliente = clienteService.obterClientePeloId(pedido.getClienteId());
+        return new PedidoResponse(cliente.getNome(), cliente.getEmail(), pedido.getDescricao(), pedido.getValorTotal());
+    }
+
+
 }
